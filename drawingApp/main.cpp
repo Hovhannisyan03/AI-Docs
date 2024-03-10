72a257f26c3750196e19c9495ae9f394274f5775
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-03-10 16:09:19 +0400

    Update main.cpp

--- drawingApp/main.cpp
+++ drawingApp/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     QWidget *ptr = nullptr;
-    DrawingCanvas obj(ptr);
+    DrawingWidget obj(ptr);
     obj.show();
     return app.exec();
 }
